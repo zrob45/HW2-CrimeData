e4db9cf8f2e0b60391969957113493e68d1410ad
pushpitab <pushpita@mit.edu>
2019-02-20 15:35:00 -0500

    hw starter code

---
+++ exercise.js
@@ -0,0 +1,56 @@
+var exercise = {};
+
+exercise.countRecords = function(data){
+    //-------------------------------------------
+    // YOUR CODE
+    // Return the total number of records
+    //-------------------------------------------
+    return 'Error: countRecords not implemented'; 
+};
+
+exercise.countDistrictCrimes = function(data,district){
+    //-------------------------------------------
+    // YOUR CODE
+    // Return the number of crimes 
+    // for the given district
+    //-------------------------------------------
+    return 'Error: countDistrictCrimes not implemented'; 
+};
+
+exercise.countPrimaryType = function(data,primaryType){
+    //-------------------------------------------
+    // YOUR CODE
+    // Return the number of crimes 
+    // for the giving primary type
+    //-------------------------------------------
+    return 'Error: countPrimaryType not implemented';
+};
+
+exercise.countLocation = function(data,location){
+    //-------------------------------------------
+    // YOUR CODE
+    // Return the number of crimes 
+    // for the given location
+    //-------------------------------------------
+    return 'Error: countLocation not implemented';
+};
+
+
+exercise.buildLatLngPoint = function(crime){
+    //-------------------------------------------
+    // YOUR CODE
+    // build the point for the given crime
+    // 
+    // For example:
+    //
+    // var point = {};    
+    // point.latitude = crime[latitudeColumnNumber];
+    // point.longitude = crime[longitudeColumnNumber];
+    // return point;    
+    //-------------------------------------------    
+    return 'Error: buildLatLngPoint not implemented';
+};
+
+
+
+
